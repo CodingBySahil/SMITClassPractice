12d21a5481d820605e29b4ab8f1f98c5b4af3118
CodingBySahil <muhammadsahil345737@gmail.com>
2023-12-12 19:00:07 +0500

    mathods in arrays

---
+++ 12 december 2023 mathods of arrays/js/app.js
@@ -0,0 +1,16 @@
+// let fruits = ['Mango','apple','banana','orange'];
+// console.log("The fruits array at the beganning");
+// console.log(fruits);
+// let fruitListOne = fruits.pop(); // pop is used to removed the last index value from the array
+// console.log(fruitListOne);
+// console.log(fruits);
+// let fruitListTwo = fruits.push("Lemon");
+// console.log(fruitListTwo)
+// let fruitListThree = fruits.shift();
+// console.log(fruitListThree);
+// let fruitListFour = fruits.unshift("Pine apple")
+
+let listOfObjects = [1,2,"apple"];
+console.log(listOfObjects)
+listOfObjects.push(prompt("Enter any value"));
+console.log(listOfObjects)
