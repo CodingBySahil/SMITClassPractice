48429573681a958fadb15292a33aa5036b52024c
CodingBySahil <muhammadsahil345737@gmail.com>
2023-12-15 13:50:35 +0500

    To do list by array mathod

---
+++ 15 december 2023 The to do list home task/js/script.js
@@ -0,0 +1,16 @@
+let toDoList =["Wake up at morning","Fresh yourself","Go for morning walk" ];
+document.ge
+
+
+function addToStart(){
+
+}
+function addToEnd(){
+
+}
+function removeFromStart(){
+
+}
+function removeFromEnd(){
+
+}
