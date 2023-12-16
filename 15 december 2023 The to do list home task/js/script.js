3d3320704f83dfd1f66114e86d0d55d37b76b56c
CodingBySahil <muhammadsahil345737@gmail.com>
2023-12-16 08:48:42 +0500

    To Do List by array mathod

--- 15 december 2023 The to do list home task/js/script.js
+++ 15 december 2023 The to do list home task/js/script.js
@@ -1,9 +1,14 @@
-let toDoList =["Wake up at morning","Fresh yourself","Go for morning walk" ];
+let toDoList =["Wake up at morning","Fresh yourself","Go for morning walk"];
 
-
-
+// for(let lengthOfList = 0 ; lengthOfList <= toDoList.length; lengthOfList++){
+//     console.log(document.getElementsByTagName('p').innerHTML = toDoList[lengthOfList])
+// }
 function addToStart(){
-
+    toDoList.push(document.querySelector(".taskTitle").innerHTML)
+    for(let lengthOfList = 0 ; lengthOfList <= toDoList.length; lengthOfList++){
+        // console.log(document.getElementsByTagName('p').innerHTML = toDoList[lengthOfList])
+        // console.log(toDoList.pop())
+    }
 }
 function addToEnd(){
 
