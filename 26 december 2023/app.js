de6fbc9a12f9e067c89ecb3c5b82305746f874a4
CodingBySahil <muhammadsahil345737@gmail.com>
2023-12-26 19:33:02 +0500

    Ternary operator

---
+++ 26 december 2023/app.js
@@ -0,0 +1,42 @@
+// Ternary operator
+// DOOR CHECKING PROGRAM
+// let isOpen = false;
+// let res;
+// isOpen ? (res = "The door is open") : (res = "The door is close");
+// console.log(res);
+
+// DECISION OF EMPIRE
+// let condition = false;
+// let decisionOfEmpire = condition ? "This is a six" : "This is out";
+// console.log(decisionOfEmpire);
+
+// let condition = true;
+// let decisionOfEmpire = ("This is ") + (condition ? "six" : "out");
+// console.log(decisionOfEmpire);
+
+// let condition = true;
+// let decisionOfEmpire = `This is ${condition ? "six" : "out"}`;
+// console.log(decisionOfEmpire);
+
+// let str = "     I       aM     waseem    ",
+//   str1 = "Hello world";
+// console.log(str.trim());
+// console.log(`The string length is ${str.length}`);
+// console.log(`The string in lower case is ${str.toLowerCase()}`);
+// console.log(`The string in upper case is ${str.toUpperCase()}`);
+// console.log(`The string in trim case is ${str.trim()}`);
+
+// let animal = "cat",
+//   str2 = "This is a ";
+// console.log(str2.concat("Red & Black ", animal));
+// console.log(`line no 31 result is : ${str2.concat("Red & Black ", animal)}`);
+
+// let fruits = "Mango Apple Banana Orange";
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+let fruits = "Mango Apple Banana Orange ";
+for (let i = 0; i < fruits.length; i++) {
+  console.log(`value on index no ${i} "${fruits[i]}"`);
+}
