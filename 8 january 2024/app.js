59afe02d0ff80dfaa2f246171ef57b5018870fa6
CodingBySahil <muhammadsahil345737@gmail.com>
2024-01-08 19:32:55 +0500

    math object

---
+++ 8 january 2024/app.js
@@ -0,0 +1,13 @@
+// let x = Math.random()*6+1;
+// console.log(Math.trunc(x))
+// // console.log(x)
+
+// Task is To print number between 10 and 20
+let n = Math.random() * 10 + 10;
+console.log(n);
+console.log("Trunck : " + Math.trunc(n)); // drop after point
+console.log("ciel : " + Math.ceil(n)); // print high value
+console.log("floor : " + Math.floor(n)); // print lower value
+console.log("Round the value : " + Math.round(n)); // print lower value
+console.log("3 power 4 is : " + Math.pow(3, 4));
+console.log("Square root of 25 is : " + Math.sqrt(25));
