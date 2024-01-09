8b04d6b49a41e544bbddb8b3f0a2fd57622a0eb4
CodingBySahil <muhammadsahil345737@gmail.com>
2024-01-09 19:48:07 +0500

    Ludo game first try

---
+++ 9 january 2024/script.js
@@ -0,0 +1,16 @@
+total = 0;
+function rollTheDisk() {
+  x = Math.random() * 6 + 1;
+  document.querySelector(".digit").innerHTML = Math.trunc(x);
+  x = Math.trunc(x);
+  let diskImg = document.querySelector(".image");
+  diskImg.src = `images/dice-${x}.png`;
+}
+//   total += x;
+//   if (total >= 100) {
+//     alert("You won the game");
+//   }
+//   document.querySelector(
+//     ".scoreBox"
+//   ).innerHTML = `Your total score is ${total}`;
+// }
