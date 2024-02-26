407f7b55ae5f2639121eb972123fe3e54860b0e4
foxlancerr <muhammadasim.code@gmail.com>
2024-02-26 17:25:52 +0500

    SMIT JS TEST

---
+++ JSSMITTest/app.js
@@ -0,0 +1,22 @@
+let body = document.querySelector("body");
+let time = document.querySelector("#time");
+let x_value = document.querySelector("#x-axis");
+let y_value = document.querySelector("#y-axis");
+
+body.addEventListener("click", (e) => {
+  getCurrentDate();
+  x_value.innerText = "X-Axis:" + e.clientX;
+  y_value.innerText = "X-Axis:" + e.clientY;
+});
+
+function getCurrentDate() {
+  let date = new Date();
+
+  time.textContent = 
+    "Time:" +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
+}
