4b955b678e7d9cff16becf326cbab008f8f469ec
CodingBySahil <muhammadsahil345737@gmail.com>
2024-04-13 09:35:00 +0500

    todo list modefied

--- 18 december 2023 todo list/app.js
+++ 18 december 2023 todo list/app.js
@@ -2,14 +2,26 @@ let toDoList = [];
 let runTimeValue;
 function add() {
   runTimeValue = document.querySelector(".title").value;
-  if (runTimeValue == ""){
-    alert("Enter a valid task !")
-  }else{
+  if (runTimeValue == "") {
+    alert("Enter a valid task !");
+  } else {
     toDoList.push(runTimeValue);
+    document.querySelector(".title").value = "";
   }
-  document.querySelector(".task").textContent = toDoList;
+  showTask();
 }
 function del() {
-  toDoList.pop(runTimeValue);
-  document.querySelector(".task").textContent = toDoList;
+  let allTask = document.querySelectorAll("h2");
+  if (allTask.length > 0) {
+    allTask[allTask.length - 1].remove();
+  } else {
+    alert("all task are delted");
+  }
 }
+
+const showTask = () => {
+  let h2 = document.createElement("h2");
+  h2.classList.add("task");
+  document.querySelector("#task").appendChild(h2);
+  h2.innerHTML = toDoList.pop();
+};
