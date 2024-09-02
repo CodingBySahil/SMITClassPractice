f88d3ab82c944f539c6c2bab56b5adfc36616452
codingBySahil <muhammadsahil345737@gmail.com>
2024-09-02 17:08:57 +0500

    first commit

---
+++ NodeJS/02class/app.mjs
@@ -0,0 +1,12 @@
+// const a = 'SMIT';
+// export default a;
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
